f510e331ba441f7a1c5a28394b5b1437f642197a
Vitaly Senokosov <6545415@gmail.com>
2018-09-04 21:12:19 +0300

    1.16 done

--- sicp/chapter1/16.rkt
+++ sicp/chapter1/16.rkt
@@ -1,2 +1,14 @@
 #lang racket/base
 (require rackunit)
+(define (fast-expt b n) 
+    (define (iter a in) 
+     (if (<= in 0) 
+       a
+       (iter (* a b) (- in 1))
+     )
+    )
+    (iter 1 n)
+ )
+   
+ (check-equal? (fast-expt 2 10) 1024)
+ (check-equal? (fast-expt 2 11) 2048)
