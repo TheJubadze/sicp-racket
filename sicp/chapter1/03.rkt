1b1a2a5e87e7e9923e3d9ee40232313b1a449b80
Vitaly Senokosov <VSenokosov@slb.com>
2019-07-25 12:16:27 +0300

    Exercise 1.3 done

---
+++ sicp/chapter1/03.rkt
@@ -0,0 +1,27 @@
+#lang racket/base
+(require rackunit)
+
+(define (sum-of-squares-of-top-two a b c)
+  (define (sqrt a)
+    (define tolerance 0.00001)
+    (define (abs x) (if (< x 0) (- x) x))
+    (define (iter guess)
+      (define newGuess (/ (+ guess (/ a guess)) 2))
+      (if (< (abs (- guess newGuess)) tolerance)
+          newGuess
+          (iter newGuess)))
+    (iter 1.0))
+
+  (define (sq x) (* x x))
+  
+  (if (> a b)
+      (if (< b c)
+          (+ (sq a) (sq b))
+          (+ (sq a) (sq c)))
+      (if (> a c)
+          (+ (sq b) (sq a))
+          (+ (sq b) (sq c)))))
+
+(check-equal? (sum-of-squares-of-top-two 1 2 3) 13)
+(check-equal? (sum-of-squares-of-top-two 3 4 2) 25)
+(check-equal? (sum-of-squares-of-top-two 5 3 4) 41)
