5c1bba477a0594ecf279d4efdd2fbb082ce1259a
Vitaly Senokosov <6545415@gmail.com>
2020-05-18 23:25:43 +0300

    Having fun with Church nums! =)

--- fsharp/SICP/Modules/ChurchNumerals.fs
+++ fsharp/SICP/Modules/ChurchNumerals.fs
@@ -3,4 +3,15 @@
 let zero = fun f -> fun x -> x
 let one = fun f -> fun x -> f x
 let two = fun f -> fun x -> f (f x)
+let three = fun f -> fun x -> f (f (f x))
+
+let rec church n = fun f -> 
+    if n <= 0 
+        then fun x -> x 
+        else fun x -> f (church (n - 1) f x)
+
+let sum n1 n2 = 
+    let compose f g = fun x -> g (f x)
+    compose n1 n2
+
 let plus1 n = fun f -> fun x -> f ((n f) x)
--- fsharp/SICP/Program.fs
+++ fsharp/SICP/Program.fs
@@ -6,10 +6,15 @@ open ChurchNumerals
 
 [<EntryPoint>]
 let main argv =
-    let s = makeSegment (makePoint 2.0 10.1) (makePoint 0.0 72.0)
-    let r = makeRect (makePoint 0.0 10.0) (makePoint 10.0 0.0)
-    let ch5 = zero plus1
-    printfn "%A" (perimeterRect r)
-    printfn "%A" (areaRect r)
-    printfn "%A" (plus1 two (fun x -> x + 1) 0)
+    //let s = makeSegment (makePoint 2.0 10.1) (makePoint 0.0 72.0)
+    //let r = makeRect (makePoint 0.0 10.0) (makePoint 10.0 0.0)
+    //let ch5 = zero plus1
+    //printfn "%A" (perimeterRect r)
+    //printfn "%A" (areaRect r)
+    
+    let f = fun x -> x + 1
+    printfn "%A" (plus1 two f 0)
+    printfn "%A" (three f 0)
+    printfn "%A" (sum (church 12 f) (church 65 f) 0)
+
     0 // return an integer exit code
